7b250e04dda16d294d7d4d3a1051b2a7deb747aa
Frode Aannevik <frode@dintero.com>
2019-09-07 21:59:52 +0200

    refactor, move oauth2 code into function

--- oauth2.bash
+++ oauth2.bash
@@ -3,46 +3,55 @@
 
 set -Eeuo pipefail
 
-opts=
-clip=0
-opts="$($GETOPT -o c -l clip -n "$PROGRAM" -- "$@")"
-err=$?
-eval set -- "$opts"
-while true; do case $1 in
-  -c|--clip) clip=1; shift ;;
-  --) shift; break ;;
-esac done
+cmd_oauth2() {
+    local opts clip=0
+    opts="$($GETOPT -o c -l clip -n "$PROGRAM" -- "$@")"
+    local err=$?
+    eval set -- "$opts"
+    while true; do case $1 in
+        -c|--clip) clip=1; shift ;;
+        --) shift; break ;;
+    esac done
 
-[[ $err -ne 0 || $# -ne 1 ]] && die "Usage: $PROGRAM $COMMAND [--clip,-  c] pass-name"
+    [[ $err -ne 0 || $# -ne 1 ]] \
+        && die "Usage: $PROGRAM $COMMAND [--clip,-  c] pass-name"
 
-path="${1%/}"
-passfile="$PREFIX/$path.gpg"
-check_sneaky_paths "$path"
+    local path="${1%/}"
+    local passfile="$PREFIX/$path.gpg"
+    check_sneaky_paths "$path"
 
-[[ -f $passfile ]] || die "Passfile not found"
+    [[ -f $passfile ]] || die "Passfile not found"
 
-url=
-refresh_token=
-curl_params=
+    local url=
+    local refresh_token=
+    local curl_params=
 
-while IFS= read -r line; do
-    [[ -z $curl_params ]] && [[ -z $refresh_token ]] && refresh_token="$line"
-    [[ -z $url && "$line" == url:* ]] && url="$(awk '{ print $2 }' <<< "$line")"
-    data="$(awk -F ': ' '{if ($1 != "url" && $2 != "") print "--data " $1 "=" $2 }' <<< "$line")"
-    curl_params+="$curl_params $data"
-done < <($GPG -d "${GPG_OPTS[@]}" "$passfile")
+    while IFS= read -r line; do
+        [[ -z $curl_params ]] && [[ -z $refresh_token ]] && refresh_token="$line"
+        [[ -z $url && "$line" == url:* ]] && url="$(awk '{ print $2 }' <<< "$line")"
+        data="$(awk -F ': ' '{if ($1 != "url" && $2 != "") print "--data " $1 "=" $2 }' \
+            <<< "$line")"
+        curl_params+="$curl_params $data"
+    done < <($GPG -d "${GPG_OPTS[@]}" "$passfile")
 
-[[ -z $url ]] && die "No url found"
-[[ -z $refresh_token ]] && die "No refresh token found"
+    [[ -z $url ]] && die "No url found"
+    [[ -z $refresh_token ]] && die "No refresh token found"
 
-# shellcheck disable=SC2086
-out=$(curl --silent ${curl_params} \
-    --data refresh_token="${refresh_token}" "${url}" \
-    | grep access_token | awk '{ print $2 }' | sed s/\"//g | sed s/,//g) \
-    || die "$path: failed to create access token."
+    local out=
+    # shellcheck disable=SC2086
+    out=$(curl --silent ${curl_params} \
+        --data refresh_token="${refresh_token}" "${url}" \
+        | grep access_token | awk '{ print $2 }' | sed s/\"//g | sed s/,//g) \
+        || die "$path: failed to create access token."
 
-if [[ $clip -ne 0 ]]; then
-    clip "$out" "access token for $path"
-else
-    echo "$out"
-fi
+    if [[ $clip -ne 0 ]]; then
+        clip "$out" "access token for $path"
+    else
+        echo "$out"
+    fi
+}
+
+case "$1" in
+  *)                     cmd_oauth2 "$@" ;;
+esac
+exit 0
